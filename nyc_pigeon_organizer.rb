e78a8262c16b9d433741920ba6b5a79c029f6149
Alejandro Aguilar Jr <alejandro.aguilar.jr@gmail.com>
2018-07-17 22:55:58 -0500

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
+require "pry"
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_hash={}
+  data.each do |characteristic, hash|
+    hash.each do |specific, array|
+      array.each do |name|
+        if !pigeon_hash.has_key?(name)
+          pigeon_hash[name]={}
+        end
+
+        if !pigeon_hash[name].has_key?(characteristic)
+          pigeon_hash[name][characteristic]=[]
+        end
+
+        if !pigeon_hash[name][characteristic].include?(specific)
+          pigeon_hash[name][characteristic].push(specific.to_s)
+        end
+      end
+    end
+  end
+  pigeon_hash
 end
